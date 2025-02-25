061a6be014dca48a5a61ee1b9f01fb4e9e652cf1
lida-skakun <kunska@gmail.com>
2025-02-25 11:42:19 +0100

    The footer component modified and styled

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,30 @@
+import facebookIcon from "../../assets/icons/facebook.svg";
+import instagramIcon from "../../assets/icons/instagram.svg";
+import pinterestIcon from "../../assets/icons/pinterest.svg";
+import "./footer.scss";
+
 export function Footer() {
-    return <h1>this is footer</h1>
+  return (
+    <div className="footer">
+      <nav className="footer_navigation">
+        <ul>
+          <li className="footer--listItems">&copy; 2025 LINEN LIFE</li>
+          <li className="footer--listItems">Terms</li>
+          <li className="footer--listItems">Sitemaps</li>
+          <li className="footer--listItems">Privacy</li>
+        </ul>
+      </nav>
+      <ul className="footer_iconList">
+        <li className="footer--listItems">
+          <img src={instagramIcon} alt="instagram" />
+        </li>
+        <li className="footer--listItems">
+          <img src={facebookIcon} alt="facebook" />
+        </li>
+        <li className="footer--listItems">
+          <img src={pinterestIcon} alt="pinterest" />
+        </li>
+      </ul>
+    </div>
+  );
 }
